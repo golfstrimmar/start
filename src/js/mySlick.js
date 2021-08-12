45457d3ee1ee1a1bad18063d3ed8c1a2c4b06c69
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2021-08-13 01:01:57 +0300

    +

--- src/js/mySlick.js
+++ src/js/mySlick.js
@@ -118,6 +118,59 @@ $(Document).ready(function() {
     });
 
 
+    $(".slider-js-posts-num").slick({
+        dots: true,
+        // arrows: false,
+        slidesToShow: 3,
+        speed: 1500,
+        easing: "ease",
+        // cssEase: "linear",
+        centerMode: false,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
+        // centerMode: true,
+        infinite: true,
+        //  initialSlide: 1,
+        // pauseOnFocus: true,
+        // pauseOnHover: true,
+        responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+    ],
+
+  customPaging: function (slider, i) {
+          var current = i + 1;
+          current = current < 10 ? "" + current : current;
+
+          var total = slider.slideCount;
+          total = total < 10 ? "" + total : total;
+
+          return (
+            '<button type="button" role="button" tabindex="0" class="slick-dots-button">\
+        		<span class="slick-dots-current">' +
+            current +
+            '</span>\
+        		<span class="slick-dots-separator">/</span>\
+        		<span class="slick-dots-total">' +
+            total +
+            "</span></button>"
+          );
+        },
+
+
+    });
+
+
 
 
     $(".slider-js-1").slick({
